2bc6ffc656b0c823fe4bdf0a5ae22dfbb22a867c
Kritikadonde <109794474+Kritikadonde@users.noreply.github.com>
2024-12-08 05:21:48 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -5,3 +5,12 @@ class Trainer:
         self.data = data
         self.model = model
         self.optimizer = optimizer
+    def train(self, iterations, batch_size, block_size):
+        for iter in range(iterations):
+            xb, yb = self.data.get_batch('train', batch_size, block_size)
+            logits, loss = self.model(xb, yb)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            if iter % 100 == 0:
+                print(f"Step {iter}, Loss: {loss.item()}")
