ac3d55aae5a17f77bbfc68b8ad29c43fa6840d0c
sakshi59 <74191300+sakshi59@users.noreply.github.com>
2024-12-09 23:24:59 -0800

    Final updated data.py

--- data.py
+++ data.py
@@ -15,5 +15,12 @@ class Data:
         n = int(0.9 * len(data))
         self.train_data = data[:n]
         self.val_data = data[n:]
+        
+        def get_batch(self, split, batch_size, block_size):
+        data = self.train_data if split == 'train' else self.val_data
+        ix = torch.randint(len(data) - block_size, (batch_size,))
+        x = torch.stack([data[i:i + block_size] for i in ix])
+        y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
+        return x, y
 
     
